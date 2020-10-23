45973c0537e2b90a214b0b26bbdead60cab32d1c
hoanglinh <linhdevios@gmail.com>
2020-10-23 22:13:32 +0700

    Initial Commit

---
+++ XTstoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XTstoreApp
+//
+//  Created by hoang linh on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XTstoreAppTests/XTstoreAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  XTstoreAppTests.swift
+//  XTstoreAppTests
+//
+//  Created by hoang linh on 10/23/20.
+//
+
+import XCTest
+@testable import XTstoreApp
+
+class XTstoreAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
